9d810bbabadb285b244253a68b6e2efbf76d04dc
FNH99 <148479741+FNH99@users.noreply.github.com>
2024-09-11 00:45:47 +0700

    change span with the other rating

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ function hideCard() {
 function rating1() {
     document.getElementById("rating-span").textContent="1";
 }
+function rating2() {
+    document.getElementById("rating-span").textContent="2";
+}
+function rating3() {
+    document.getElementById("rating-span").textContent="3";
+}
+function rating4() {
+    document.getElementById("rating-span").textContent="4";
+}
+function rating5() {
+    document.getElementById("rating-span").textContent="5";
+}
